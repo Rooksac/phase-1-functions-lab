f91dfc56ff0577b9b2a9c5fbc00413ba90a8e099
Zachary Dean <Zakedoubled@gmail.com>
2022-07-19 09:33:50 -0400

    commit tree 48e6cb445a500ac229e49d4468a669ae2b29004e

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    if (someValue > 42) {
+        return (someValue - 42);
+    }
+    else if (someValue < 42) {
+        return (42 - someValue)
+    }    
+}
+function distanceFromHqInFeet(someValue) {
+    return (distanceFromHqInBlocks(someValue) * 264);
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return ((start - destination) * 264)
+    }
+    else if (destination > start) {
+        return ((destination - start) * 264)
+    }
+}
+function calculatesFarePrice(start, destination) {
+    const fareCalc = distanceTravelledInFeet(start, destination);
+    if (fareCalc <= 400) {
+        return 0
+    }
+    else if (fareCalc > 400 && fareCalc <= 2000){
+        return ((fareCalc - 400) * .02)
+    }
+    else if (fareCalc > 2000 && fareCalc <=2500) {
+        return 25
+    }
+    else if (fareCalc > 2500) {
+        return "cannot travel that far"
+    }
+}
